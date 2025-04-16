ad6af2b5323bc768f91c1b0056e1fa3143357aa7
Rohan Rahim <rohanrahim024@gmail.com>
2025-04-16 10:47:56 +0600

    Combination sum 2

--- Data Structures/Recursion/Combination Sum 2/code.cpp
+++ Data Structures/Recursion/Combination Sum 2/code.cpp
@@ -1,34 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void getAllCombinations(vector<int>& candidates, int target, vector<int>& combination, set<vector<int>>& s, int i) {
+void getAllCombinations(vector<int>& candidates, int target, vector<int>& combination, vector<vector<int>>& ans, int i) {
 	if(target == 0) {
-		s.insert(combination);
+		ans.push_back(combination);
 		return;
 	}
 	if(i == candidates.size() || target < 0) return; 
 
 	//include an element
 	combination.push_back(candidates[i]);
-	getAllCombinations(candidates, target-candidates[i], combination, s, i+1);
+	getAllCombinations(candidates, target-candidates[i], combination, ans, i+1);
 	//exclude
 	combination.pop_back();
 	int idx = i+1;
-	while(idx < candidates.size() && candidates[i] == candidates[i-1]) idx++; //skip the duplicates
-	getAllCombinations(candidates, target, combination, s, idx);
+	while(idx < candidates.size() && candidates[idx] == candidates[idx-1]) idx++; //skip the duplicates
+	getAllCombinations(candidates, target, combination, ans, idx);
 }
 
 vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
 	sort(candidates.begin(), candidates.end());
-    set<vector<int>> s;
+    vector<vector<int>> ans;
     vector<int> combination;
-    getAllCombinations(candidates, target, combination, s, 0);
+    getAllCombinations(candidates, target, combination, ans, 0);
 
-    vector<vector<int>> ans(s.begin(), s.end());
     return ans;
 }
 
 int main() {
+	#ifndef OJ
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
 	vector<int> v = {10, 1, 2, 7, 6, 1, 5};
 
 	vector<vector<int>> ans = combinationSum2(v, 8);
