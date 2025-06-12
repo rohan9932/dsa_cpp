12ca6ea6dfce44d03058aa5418b47a373c8eeec2
Rohan Rahim <rohanrahim024@gmail.com>
2025-06-12 18:14:25 +0600

    Iterative Inorder and Preorder

--- Data Structures/Binary Tree/bintree.cpp
+++ Data Structures/Binary Tree/bintree.cpp
@@ -131,12 +131,60 @@ void inorder(Node* root) {
 	inorder(root->right);
 }
 
+void inorderIterative(Node* root) {
+	//1.Push the root to the stack and start going to left (while the node is not NULL)
+	//2.If we get NULL then return to root and print by printing the top node on stack (then pop it)
+	//3. Move to right
+	//4. If at any point stack is empty then traversal is over and break the loop
+	if(root == NULL) return;
+
+	stack<Node*> s;
+	Node* temp = root;
+
+	while(true) {
+		if(temp != NULL) {
+			s.push(temp);
+			temp = temp->left;
+		} else {
+			if(s.empty()) break;
+			temp = s.top();
+			s.pop();
+			cout << temp->data << " ";
+			temp = temp->right;
+		}
+	}
+}
+
 void preorder(Node* root) {
 	if(root == NULL) return;
 
 	cout << root->data << " ";
 	preorder(root->left);
 	preorder(root->right);
+}
+
+void preorderIterative(Node* root) {
+	//1. Take a stack. Push root to it
+	//2. like level order traversal traverse while stack is not empty
+	//3. take the top element(which is the root of that subtree) and pop it from stack. 
+	//4. print the root
+	//5. push the right and then left element of the root in the stack
+	if(root == NULL) return;
+
+	stack<Node*> s;
+	s.push(root);
+	while(!s.empty()) {
+		Node* root = s.top();
+		s.pop();
+		cout << root->data << " ";
+		//first right then left (As stack is LIFO(reversed output))
+		if(root->right) {
+			s.push(root->right);
+		}
+		if(root->left) {
+			s.push(root->left);
+		}
+	}
 }
 
 void postorder(Node* root) {
